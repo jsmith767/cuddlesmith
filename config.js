c7784da5ea43eb017645cd2c12858aad6187462c
Julian <cloudsmithsdatascience@gmail.com>
2025-12-25 02:19:19 -0800

    Add config.js for Cloudflare Pages deployment

---
+++ config.js
@@ -0,0 +1,7 @@
+// Config file for Google Calendar API
+// Note: API key is visible client-side, but should be restricted in Google Cloud Console
+const CONFIG = {
+    googleCalendarApiKey: 'AIzaSyDlxDpEppASwDoM9aolvXtg2GdfnqQaDJA',
+    calendarId: 'e98c8cdd52ee5115c60ad53513496e3495da75b3595e14730a4e295bc17a908d@group.calendar.google.com'
+};
+
